1deddafff12f78850d63e845f9ac841747c3215b
Marcin Binek <binekmarcin@gmail.com>
2017-12-27 16:10:35 +0100

    1 string

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        String priceMessage = "Amount due " + "$" + quantity * 5;
         displayMessage(priceMessage);
     }
 
